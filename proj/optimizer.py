fix gurobi precision issue

proj/optimizer.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 class Optimizer(ABC):
 
-    TIMEOUT=2700 # 24hrs of cpu time
+    TIMEOUT=60 # 24hrs of cpu time
     
     @abstractproperty
     def results(self):
@@ -18,6 +18,8 @@ class Optimizer(ABC):
     @staticmethod
     def create_results(constraints, weights, method_name , time_series):
         violated = constraints.mul(weights).sum(axis=1).gt(-1.0)
+        # correct for float inprecision 
+        time_series['obj_val'] = np.maximum(violated.sum(), time_series['obj_val'].values)
         hist = violated.groupby(level=0).sum().value_counts()
         weights.index = [str(x) for x in weights.index]
         return {
@@ -63,4 +65,4 @@ class Optimizer(ABC):
     @staticmethod
     def post_process_boost_map(boost_map):
         boost_map = boost_map.where(boost_map > 0).dropna()
-        return boost_map / boost_map.min()
+        return boost_map 

proj/torch_optimizer.py
@@ -48,7 +48,7 @@ class BoostModel(nn.Module):
 
 class TorchOptimizer(Optimizer):
 
-    def __init__(self, iters=100, step_interval=25, timeout=Optimizer.TIMEOUT):
+    def __init__(self, iters=500, step_interval=25, timeout=Optimizer.TIMEOUT):
         self._timeout = timeout
         self._iters = iters
         # number iterations per round (gradient updates)
@@ -67,7 +67,7 @@ class TorchOptimizer(Optimizer):
     def optimize(self, constraints):
         timer = Timer()
         X = torch.Tensor(constraints.values)
-        label = -torch.ones(len(constraints), dtype=torch.float32)
+        label = -torch.ones(len(constraints), dtype=torch.float64)
         cands = []
         self._results['setup_time'] = timer.get_total()
         while timer.get_total() < self._timeout: