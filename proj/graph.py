f45f4b873e352a13f8373f327ccb157e9938925f
derekpaulsen <derekpaulsensi@gmail.com>
2022-04-28 11:58:41 -0500

    bug found results

--- proj/graph.py
+++ proj/graph.py
@@ -14,13 +14,15 @@ TORCH = DATA_DIR / 'torch.json'
 argp = ArgumentParser()
 argp.add_argument('--show_graph', action='store_true')
 
-
+TIME_LIMIT = 2700
 DF = None
 
-def process_time_series(x):
+def process_time_series(row):
 
-    s = pd.DataFrame(x).astype(np.int32).set_index('time')['obj_val']
+    s = pd.DataFrame(row['time_series']).astype(np.int32).set_index('time')['obj_val']
     s = s.groupby(s.index).last()
+    s = s.loc[s.index < TIME_LIMIT]
+    s[TIME_LIMIT] = row['total_violated']
     return s
 
 
@@ -29,21 +31,21 @@ def read_data(f):
         df = pd.DataFrame([json.loads(x) for x in ifs if len(x) > 1]).set_index('dataset')
 
     method_name = df['method_name'].iloc[0]
-    df['time_series'] = [process_time_series(x).rename(method_name) for x in df['time_series'].values]
+    df['time_series'] = [process_time_series(x[1]).rename(method_name) for x in df.iterrows()]
 
     return df
 
-def print_stats(df, ds):
-    if 'MILP' in df.columns and df['MILP'].lt(df['torch']).any():
-        t_overtake = df['MILP'].lt(df['torch']).idxmax()
+def print_stats(ts, ds):
+    if 'MILP' in ts.columns and ts['MILP'].lt(ts['torch']).any():
+        t_overtake = ts['MILP'].lt(ts['torch']).idxmax()
     else:
         t_overtake = np.nan
 
     print(ds)
     print(f'time for MILP to overtake torch : {t_overtake}')
-    print(f'time for MILP to reach best value: {df.MILP.idxmin()}')
-    print(f'time for torch to reach best value: {df.torch.idxmin()}')
-    print()
+    print(f'time for MILP to reach best value: {ts.MILP.idxmin()}')
+    print(f'time for torch to reach best value: {ts.torch.idxmin()}')
+    print(ts)
 
 def graph(milp, torch, show):
     print(milp.index)
--- proj/optimizer.py
+++ proj/optimizer.py
@@ -17,7 +17,7 @@ class Optimizer(ABC):
     
     @staticmethod
     def create_results(constraints, weights, method_name , time_series):
-        violated = constraints.mul(weights).sum(axis=1).sub(1.0).gt(0.0)
+        violated = constraints.mul(weights).sum(axis=1).gt(-1.0)
         hist = violated.groupby(level=0).sum().value_counts()
         weights.index = [str(x) for x in weights.index]
         return {
